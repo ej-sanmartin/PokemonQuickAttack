f99d7a66ca9abb3d4de525c59cf6dc358a32ca13
Edgar J San Martin <esanmartin@google.com>
2022-12-03 04:44:48 -0500

    Nit refactor. Adhere to RESTful API conventions.

--- flaskr/app.py
+++ flaskr/app.py
@@ -12,13 +12,13 @@ def index():
     return render_template("index.html.jinja",
                            search=Search.enum_to_string(search))
 
-@app.route("/search_type", methods=['POST'])
-def change_search_type():
+@app.route("/search-type", methods=['POST'])
+def search_type():
     global search
     # Prevents users from sending multiple requests that would cause an unneeded
     # re render. For instance, if the page is expecting a pokemon name, having
     # the user repeatedly press the "pokemon" button will re render the page.
-    # The page should only be re rendered if its changing the search type.
+    # The page should only be re rendered if it's changing the search type.
     if (request.form.get('search') == Search.enum_to_string(search)):
         pass
 
